c5a7ec6743ac8dd7127072b9ce526b37fb337f06
comradvr <wmason678@gmail.com>
2025-03-31 18:51:15 -0600

    Very important changes

---
+++ src/main/java/net/spaxe/testmod/block/custom/MagicBlock.java
@@ -0,0 +1,11 @@
+package net.spaxe.testmod.block.custom;
+
+import net.minecraft.world.level.block.Block;
+
+public class MagicBlock extends Block {
+    public MagicBlock(Properties properties) {
+        super(properties);
+    }
+
+
+}
