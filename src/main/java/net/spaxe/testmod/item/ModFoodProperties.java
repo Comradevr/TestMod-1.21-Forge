6101a991dd8c1276722e39187c6725e78808e94c
comradvr <wmason678@gmail.com>
2025-04-01 23:47:35 -0600

    Very important changes

--- src/main/java/net/spaxe/testmod/item/ModFoodProperties.java
+++ src/main/java/net/spaxe/testmod/item/ModFoodProperties.java
@@ -8,7 +8,7 @@ public class ModFoodProperties {
     public static final FoodProperties KOHLRABI = new FoodProperties.Builder().nutrition(7).saturationModifier(1f)
             .effect(new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 3600, 10), 1000f)
             .fast().effect(new MobEffectInstance(MobEffects.REGENERATION, 3600, 1000000000), 100f)
-            .effect(new MobEffectInstance(MobEffects.ABSORPTION 20), 100f)
+            .effect(new MobEffectInstance(MobEffects.ABSORPTION, 20), 100f)
             .effect(new MobEffectInstance(MobEffects.DAMAGE_BOOST, 3600, 10000), 100f)
             .effect(new MobEffectInstance(MobEffects.FIRE_RESISTANCE, 3600), 1000)
             .effect(new MobEffectInstance(MobEffects.DIG_SPEED, 3600, 1000), 100f).build();
