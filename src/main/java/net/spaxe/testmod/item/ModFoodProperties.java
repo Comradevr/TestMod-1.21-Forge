0d0a833b3e3c3c4107836aa2a9b0057b94c3bb0f
comradvr <wmason678@gmail.com>
2025-04-02 19:47:35 -0600

    Very important changes

--- src/main/java/net/spaxe/testmod/item/ModFoodProperties.java
+++ src/main/java/net/spaxe/testmod/item/ModFoodProperties.java
@@ -5,13 +5,14 @@ import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.food.FoodProperties;
 
 public class ModFoodProperties {
-    public static final FoodProperties KOHLRABI = new FoodProperties.Builder().nutrition(7).saturationModifier(1f)
-            .effect(new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 3600, 10), 1000f)
+    public static final FoodProperties KOHLRABI = new FoodProperties.Builder().nutrition(1).saturationModifier(1f)
+            .effect(new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 3600, 6), 100f)
             .fast().effect(new MobEffectInstance(MobEffects.REGENERATION, 3600, 1000000000), 100f)
-            .effect(new MobEffectInstance(MobEffects.ABSORPTION, 20), 100f)
-            .effect(new MobEffectInstance(MobEffects.DAMAGE_BOOST, 3600, 10000), 100f)
-            .effect(new MobEffectInstance(MobEffects.FIRE_RESISTANCE, 3600), 1000)
-            .effect(new MobEffectInstance(MobEffects.DIG_SPEED, 3600, 1000), 100f).build();
+            .effect(new MobEffectInstance(MobEffects.ABSORPTION, 3600, 50), 100f)
+            .effect(new MobEffectInstance(MobEffects.DAMAGE_BOOST, 3600, 1000000000), 100f)
+            .effect(new MobEffectInstance(MobEffects.FIRE_RESISTANCE, 3600), 100f)
+            .effect(new MobEffectInstance(MobEffects.DIG_SPEED, 3600, 100000), 100f)
+            .effect(new MobEffectInstance(MobEffects.DOLPHINS_GRACE, 3600, 1000000), 100f).build();
 
 
 }
